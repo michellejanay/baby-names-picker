3d523d549d4f38c7edcff5c7d9951658732988e2
michelle <mindyourphotosynthesis@gmail.com>
2022-12-30 17:09:05 +0000

    search: bug fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './App.css'
 import NamesList from './components/NamesList'
 // import SearchBar from './components/SearchBar'
@@ -6,30 +6,22 @@ import NameData from './data/babyNamesData.json'
 NameData.sort((a, b) => (a.name < b.name ? -1 : a.name > b.name ? 1 : 0))
 
 function App() {
-  const [babyNames, setBabyNames] = useState(NameData)
   const [search, setSearch] = useState('')
 
   const handleChange = (e) => {
     e.preventDefault()
     setSearch(e.target.value)
-    console.log(e.target.value)
-
-    let names = babyNames.filter((name) =>
-      search === '' ? babyNames : name.name.toLowerCase().includes(search.toLowerCase())
-    )
-    
-
-    setBabyNames(() => names)
-    console.log(names)
   }
 
-  useEffect(() => {}, [])
+  let names = NameData.filter((name) =>
+      name.name.toLowerCase().includes(search.toLowerCase())
+    )
 
   return (
     <div className="App">
       {/* <SearchBar /> */}
       <input type="search" value={search} onChange={handleChange} />
-      <NamesList babyNames={babyNames} />
+      <NamesList babyNames={names} />
     </div>
   )
 }
